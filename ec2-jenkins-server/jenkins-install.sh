61012cd9bcfc945d3ee1a5ef50d22f8ba030db36
Arif Sadiq <arifdevopstech@hotmail.com>
2024-07-12 11:08:23 +0400

    update jenkins install file

--- ec2-jenkins-server/jenkins-install.sh
+++ ec2-jenkins-server/jenkins-install.sh
@@ -14,11 +14,6 @@ sudo systemctl start jenkins
 # install git
 sudo yum install git -y
 
-# install terraform
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
-
 # install kubectl
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
